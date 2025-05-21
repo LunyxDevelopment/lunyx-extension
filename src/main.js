3bc756ad94e63e580581656a29541a5d0d41fe9e
rugveddanej <rugveddanej.socialmedia@gmail.com>
2025-05-21 22:26:07 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import plugin from '../plugin.json';
+
+class Lunyx {
+
+  async init() {
+    // plugin initialisation 
+  }
+
+  async destroy() {
+    // plugin clean up
+  }
+}
+
+if (window.acode) {
+  const LunyxExtension = new Lunyx();
+  acode.setPluginInit(plugin.id, async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+    if (!baseUrl.endsWith('/')) {
+      baseUrl += '/';
+    }
+    LunyxExtension.baseUrl = baseUrl;
+    await LunyxExtension.init($page, cacheFile, cacheFileUrl);
+  });
+  acode.setPluginUnmount(plugin.id, () => {
+    LunyxExtension.destroy();
+  });
+}
